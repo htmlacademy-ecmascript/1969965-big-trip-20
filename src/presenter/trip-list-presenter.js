7f313b7fa6fe985732a7e7edb1167de67e61a172
JulianaAstra <julianaas@yandex.ru>
2023-05-11 22:42:49 +0300

    добавит еще приватных полей

--- src/presenter/trip-list-presenter.js
+++ src/presenter/trip-list-presenter.js
@@ -14,6 +14,11 @@ import { RenderPosition } from '../framework/render.js';
 export default class TripListPresenter {
   #tripListContainer;
   #tripsModel;
+  #trips;
+  #offers;
+  #destinations;
+  #destinationsList;
+  #eventFormDestinationComponent;
 
   #tripListComponent = new TripListView();
   #eventFormComponent = new EventFormView();
@@ -26,25 +31,25 @@ export default class TripListPresenter {
   }
 
   init() {
-    this.trips = [...this.#tripsModel.trips];
-    this.offers = [...this.#tripsModel.offers];
-    this.destinations = [...this.#tripsModel.destinations];
-    this.destinationsList = [...this.#tripsModel.destinationsList];
-    this.eventFormDestinationComponent = new EventFormDestinationView({trip: this.trips[0], destinations: this.destinations});
+    this.#trips = [...this.#tripsModel.trips];
+    this.#offers = [...this.#tripsModel.offers];
+    this.#destinations = [...this.#tripsModel.destinations];
+    this.#destinationsList = [...this.#tripsModel.destinationsList];
+    this.#eventFormDestinationComponent = new EventFormDestinationView({trip: this.#trips[0], destinations: this.#destinations});
 
     render(this.#tripListComponent, this.#tripListContainer);
 
-    for (let i = 1; i < this.trips.length; i++) {
-      render(new TripItemView({trip: this.trips[i], offers: this.offers, destinations: this.destinations}), this.#tripListComponent.element);
+    for (let i = 1; i < this.#trips.length; i++) {
+      render(new TripItemView({trip: this.#trips[i], offers: this.#offers, destinations: this.#destinations}), this.#tripListComponent.element);
     }
 
     render(this.#eventFormComponent, this.#tripListComponent.element, RenderPosition.AFTERBEGIN);
 
-    render(new EventFormHeaderView({destinationsList: this.destinationsList, trip: this.trips[0], destinations: this.destinations}), this.#eventFormComponent.getChildElement(0));
+    render(new EventFormHeaderView({destinationsList: this.#destinationsList, trip: this.#trips[0], destinations: this.#destinations}), this.#eventFormComponent.getChildElement(0));
     render(this.#eventFormDetailsComponent, this.#eventFormComponent.getChildElement(0));
     render(this.#eventFormOffersComponent, this.#eventFormDetailsComponent.element);
-    render(this.eventFormDestinationComponent, this.#eventFormDetailsComponent.element);
-    render(new EventFormOfferItemView({offers: this.offers, trip: this.trips[0], destinations: this.destinations}), this.#eventFormOffersComponent.element);
-    render(new EventFormDestinationPictureView({trip: this.trips[0], destinations: this.destinations}), this.eventFormDestinationComponent.element);
+    render(this.#eventFormDestinationComponent, this.#eventFormDetailsComponent.element);
+    render(new EventFormOfferItemView({offers: this.#offers, trip: this.#trips[0], destinations: this.#destinations}), this.#eventFormOffersComponent.element);
+    render(new EventFormDestinationPictureView({trip: this.#trips[0], destinations: this.#destinations}), this.#eventFormDestinationComponent.element);
   }
 }
