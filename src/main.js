16142b65de3c3e0025d41b96584ee868c497ccc5
JulianaAstra <julianaas@yandex.ru>
2023-06-13 23:20:31 +0300

    проверка строки

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import TripsModel from './modell/trips-model.js';
 import FilterModel from './modell/filter-model.js';
 import TripsApiService from './trips-api-service.js';
 
-const AUTHORIZATION = 'Basic d7hgj398fll45dfg';
+const AUTHORIZATION = 'Basic d7hgj398fl4g8d5gdf9dhj5hfjwl45dfg';
 const END_POINT = 'https://20.ecmascript.pages.academy/big-trip';
 
 const infoHeaderElement = document.querySelector('.trip-main');
